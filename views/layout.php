2fbd3f85b4c2f41f2c56c13cfef8dc960610b5c1
Eric Barnes <eric@ericlbarnes.com>
2012-02-09 09:09:04 -0500

    Added layout view

---
+++ views/layout.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Language Builder</title>
+		<?php echo HTML::style('bundles/langbuilder/langbuilder.css'); ?>
+	</head>
+	<body>
+		<div id="container" class="clearfix">
+			<aside>
+				<p>Navigation</p>
+			</aside>
+			<section>
+				<h1>Form</h1>
+
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+				<p>TEXT</p>
+
+			</section>
+		</div>
+
+		<footer>
+
+		</footer>
+
+	</body>
+</html>
