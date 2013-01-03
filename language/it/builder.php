070d1a5a7e54dcd6d9c12254097f252a44489634
Pasvaz <pasqualevazzana@gmail.com>
2013-01-03 17:11:45 +0100

    Added italian translation

---
+++ language/it/builder.php
@@ -0,0 +1,12 @@
+<?php 
+return array(
+	'title' => 'Laravel Language Builder',
+	'please_select' => 'Seleziona per favore',
+	'intro_message' => 'Per iniziare seleziona il linguaggio che vuoi tradurre',
+	'translate' => 'Traduci',
+	'select_file' => 'Seleziona il file che vuoi tradurre',
+	'save_changes' => 'Salva cambiamenti',
+	'toggle_translated' => 'Marca come tradotto',
+	'missing_translations' => 'Traduzioni mancanti',
+	'all_translation_files' => 'Tutte le traduzioni',
+);
